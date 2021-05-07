ad55901ae6264afed09366f34beba631b1034212
Eccleastic <eccleastic@gmail.com>
2021-05-07 14:05:50 +0200

    Update Worker.js

--- lab7/Worker.js
+++ lab7/Worker.js
@@ -15,7 +15,7 @@ self.onmessage = function (JSONData) {
     console.log(JSONData.data);
     var parseData = JSON.parse(JSONData.data);
     Object.keys(parseData).forEach(function (key) {
-        var newData;
+        var newData = parseData[key];
             for (i = 0; i < parseData[key].length; i++) {
                 if (parseData[key][i] == parseData[key][i].toLowerCase()) {
                     newData[i] = parseData[key][i].toUpperCase();
